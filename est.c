9c0a3d8b900dd1552fcdcfbd6ec0908895bdec6c
prime number <primenumber.proj@gmail.com>
2012-07-24 05:12:37 +0900

    modified error of non C99 env

--- est.c
+++ est.c
@@ -30,21 +30,27 @@ void memsetex(void* dst,void* data,size_t nmemb,size_t size)
 int main()
 {
 	int n;
-	scanf("%d",&n);
-	const int lendiv8 = n/30+1;
-	const int len = lendiv8*8;
-	int* list = mem(int,len*8);
+	const int lendiv8;
+	const int len;
+	int *list;
 	int i,j,k,l;
 	int one = 1;
+	scanf("%d",&n);
+	lendiv8 = n/30+1;
+	len = lendiv8*8;
+	list = mem(int,len*8);
 	memsetex(list,&one,sizeof(int),len);
 	for(i = 0;i < lendiv8;i++)
 	{
 		for(j = 0;j < 8;j++)
 		{
+			const int p;
+			const int invp;
+			const int loop;
 			if(!i && !j)continue;
-			const int p = i*30+table[j];
-			const int invp = inv[j][0] - i * inv[j][1];
-			const int loop = lendiv8 / p;
+			p = i*30+table[j];
+			invp = inv[j][0] - i * inv[j][1];
+			loop = lendiv8 / p;
 			for(k = 0;k < 8;k++)
 			{
 				int depth = -table[k]*invp;
